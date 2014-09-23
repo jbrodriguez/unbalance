cba42f09b9ac4ed13135dad27296061f66918626
Juan B. Rodriguez <jbrodriguez@apertoire.net>
2014-09-23 18:23:17 -0500

    - Added html5 localStorage functionality

--- client/app/core/core.module.js
+++ client/app/core/core.module.js
@@ -10,7 +10,7 @@
         /*
          * Our reusable cross app code modules
          */
-        'blocks.exception', 'blocks.logger', /*'blocks.router',*/
+        'blocks.exception', 'blocks.logger', 'blocks.storage', /*'blocks.router',*/
         /*
          * 3rd Party modules
          */
