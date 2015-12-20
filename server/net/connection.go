5b883188c41947a46bd02746ee4b7d036c7f17cd
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-12-20 07:20:39 -0500

    (back) Define new connection struct semantics

---
+++ server/net/connection.go
@@ -0,0 +1,41 @@
+package net
+
+import (
+	"golang.org/x/net/websocket"
+	"jbrodriguez/unbalance/server/dto"
+)
+
+type MessageFunc func(message *dto.Packet)
+type CloseFunc func(conn *Connection, err error)
+
+type Connection struct {
+	id        string
+	ws        *websocket.Conn
+	onMessage MessageFunc
+	onClose   CloseFunc
+}
+
+func NewConnection(ws *websocket.Conn, onMessage MessageFunc, onClose CloseFunc) *Connection {
+	return &Connection{
+		ws:        ws,
+		onMessage: onMessage,
+		onClose:   onClose,
+	}
+}
+
+func (c *Connection) Read() {
+	for {
+		var packet dto.Packet
+		if err := websocket.Message.Receive(c.ws, &packet); err != nil {
+			go c.onClose(c, err)
+			break
+		} else {
+			go c.onMessage(&packet)
+		}
+	}
+}
+
+func (c *Connection) Write(packet *dto.Packet) (err error) {
+	err = websocket.Message.Send(c.ws, packet)
+	return
+}
