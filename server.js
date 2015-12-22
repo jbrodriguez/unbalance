10706f8fc896613c09a0cb407ca52fa28f4b7889
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-12-22 11:42:11 -0500

    (front) Add http proxy for api

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack.config.js';
+import httpProxy from 'http-proxy'
+
+
+// We need to add a configuration to our proxy server,
+// as we are now proxying outside localhost
+var proxy = httpProxy.createProxyServer({
+  changeOrigin: true
+});
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
+
 const app = express();
 
 if (isDeveloping) {
@@ -27,10 +36,18 @@ if (isDeveloping) {
 
 	app.use(middleware);
 	app.use(webpackHotMiddleware(compiler));
+
+	app.all('/api/*', function(req, res) {
+		proxy.web(req, res, {
+			target: 'http://hal.apertoire.org:6237'
+		})
+	})
+
 	app.get('*', function response(req, res) {
 		res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
 		res.end();
 	});
+
 } else {
 	app.use(express.static(__dirname + '/dist'));
 	app.get('*', function response(req, res) {
