515dca35ca6a6306c0ff89f639398779a827bc40
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-12-23 06:59:16 -0500

    (front) Define required constants

--- client/constant.js
+++ client/constant.js
@@ -1,9 +1,11 @@
-export const START = "START"
+export const START = 'START'
 export const GET_CONFIG = 'GET_CONFIG'
 export const GOT_CONFIG = 'GOT_CONFIG'
 export const ADD_FOLDER = 'ADD_FOLDER'
 export const FOLDER_ADDED = 'FOLDER_ADDED'
 export const OP_IN_PROGRESS = 'OP_IN_PROGRESS'
+export const GET_STORAGE = 'GET_STORAGE'
+export const GOT_STORAGE = 'GOT_STORAGE'
 export const CALCULATE = 'CALCULATE'
 export const MOVE = 'MOVE'
 export const TOGGLE_DRY_RUN = 'TOGGLE_DRY_RUN'
