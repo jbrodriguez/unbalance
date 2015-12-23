820567bd209cf295adc13cd4948d5e6a42108596
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-12-23 06:58:58 -0500

    (back) Remove runningInDocker flag. All it did was to locate mdcmd, we now move it to the appropriate location when run under Docker. Also, diskmv will be put alongside unbalance binary, so that it will always be found there (back) Define config flag in code to pick up any settings saved by the user (back) Fix save settings logic. We now write to a tmp file and rename it when all writes are done (back) Add addFolder functionality

--- server/lib/settings.go
+++ server/lib/settings.go
@@ -4,7 +4,6 @@ import (
 	// "errors"
 	"fmt"
 	"github.com/namsral/flag"
-	"io"
 	"os"
 	"strings"
 )
@@ -27,15 +26,14 @@ type Config struct {
 type Settings struct {
 	Config
 
-	Conf            string
-	Log             string
-	RunningInDocker bool
-	ReservedSpace   int64
+	Conf          string
+	Log           string
+	ReservedSpace int64
 }
 
 func NewSettings(version string) (*Settings, error) {
 	var config, log, folders string
-	var dryRun, runningInDocker bool
+	var dryRun bool
 	var notifyCalc, notifyMove int
 
 	// location := SearchFile(name, locations)
@@ -44,17 +42,17 @@ func NewSettings(version string) (*Settings, error) {
 	// }
 
 	// /boot/config/plugins/unbalance/
-	flag.StringVar(&config, "config", "/boot/config/plugins/unbalance/unbalance.conf", "config location")
+	flag.StringVar(&config, "config", "", "config location")
 	flag.StringVar(&log, "log", "", "folder where log file will be written to")
 	flag.StringVar(&folders, "folders", "", "folders that will be scanned for media")
 	flag.BoolVar(&dryRun, "dryRun", true, "perform a dry-run rather than actual work")
 	flag.IntVar(&notifyCalc, "notifyCalc", 0, "notify via email after calculation operation has completed (unraid notifications must be set up first): 0 - No notifications; 1 - Simple notifications; 2 - Detailed notifications")
 	flag.IntVar(&notifyMove, "notifyMove", 0, "notify via email after move operation has completed (unraid notifications must be set up first): 0 - No notifications; 1 - Simple notifications; 2 - Detailed notifications")
-	flag.BoolVar(&runningInDocker, "docker", false, "notify via email after move operation has completed (unraid notifications must be set up first)")
 
+	flag.Set("config", "/boot/config/plugins/unbalance/unbalance.conf")
 	flag.Parse()
 
-	// fmt.Printf("mediaFolders: %s\n", mediaFolders)
+	// fmt.Printf("folders: %s\nconfig: %s\n", folders, config)
 
 	s := &Settings{}
 	if folders == "" {
@@ -69,45 +67,36 @@ func NewSettings(version string) (*Settings, error) {
 
 	s.Conf = config
 	s.Log = log
-	s.RunningInDocker = runningInDocker
 	s.ReservedSpace = RESERVED_SPACE
 
 	return s, nil
 }
 
-func (s *Settings) Save() error {
-	file, err := os.Create(s.Conf)
-	defer file.Close()
-
-	if err != nil {
-		return err
-	}
-
-	// if err = writeLine(file, "datadir", s.DataDir); err != nil {
-	// 	return err
-	// }
-
-	// if err = writeLine(file, "webdir", s.WebDir); err != nil {
-	// 	return err
-	// }
-
-	// if err = writeLine(file, "logdir", s.LogDir); err != nil {
-	// 	return err
-	// }
-
-	// mediaFolders := strings.Join(s.MediaFolders, "|")
-	// if err = writeLine(file, "mediafolders", mediaFolders); err != nil {
-	// 	return err
-	// }
-
-	return nil
+func (s *Settings) AddFolder(folder string) {
+	s.Folders = append(s.Folders, folder)
 }
 
-func writeLine(file *os.File, key, value string) error {
-	_, err := io.WriteString(file, fmt.Sprintf("%s=%s\n", key, value))
-	if err != nil {
+func (s *Settings) Save() (err error) {
+	tmpFile := s.Conf + ".tmp"
+
+	folders := strings.Join(s.Folders, "|")
+	if err = WriteLine(tmpFile, fmt.Sprintf("folders=%s", folders)); err != nil {
 		return err
 	}
 
-	return nil
+	if err = WriteLine(tmpFile, fmt.Sprintf("dryRun=%b", s.DryRun)); err != nil {
+		return err
+	}
+
+	if err = WriteLine(tmpFile, fmt.Sprintf("notifyCalc=%d", s.NotifyCalc)); err != nil {
+		return err
+	}
+
+	if err = WriteLine(tmpFile, fmt.Sprintf("notifyMove=%d", s.NotifyCalc)); err != nil {
+		return err
+	}
+
+	os.Rename(tmpFile, s.Conf)
+
+	return
 }
