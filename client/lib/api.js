d66fd85260db5e09823272cc98d5221bba24e535
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-12-23 06:59:45 -0500

    (front) Add getstorage api call

--- client/lib/api.js
+++ client/lib/api.js
@@ -12,10 +12,18 @@ export default class Api {
 	}
 
 	addFolder(folder) {
+		// console.log('sending: ', folder)
 		return fetch(this.hostr + '/config/folder', {
 			method: 'PUT',
-			body: JSON.stringify({topic: "", payload: folder})
+			headers: {'content-type': 'application/json'},
+			body: JSON.stringify({payload: folder})
 		})
 		.then(resp => resp.json())
+	}
+
+	getStorage() {
+		// console.log('this.hostr', this.hostr)
+		return fetch(this.hostr + '/storage')
+			.then(resp => resp.json())
 	}
 }
