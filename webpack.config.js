7f57223f8f63a1737372913f97101c4414aff1da
Juan B. Rodriguez <jbrodriguez@gmail.com>
2015-12-31 12:55:37 -0500

    (front) Define module dependencies (front) Redefine building steps for js files, using babel 6 logic

--- webpack.config.js
+++ webpack.config.js
@@ -28,15 +28,19 @@ module.exports = {
 	],
 	module: {
 		loaders: [{
-			test: /\.js?$/,
+			loader: 'babel-loader',
 			exclude: /node_modules/,
-			loader: 'babel'
+			test: /\.js$/,
+			query: {
+				plugins: ['transform-runtime'],
+				presets: ['es2015', 'react']
+			}
 		}, {
 			test: /\.json?$/,
 			loader: 'json'
 		}, { 
 			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-			loader: "url-loader?limit=10000&minetype=application/font-woff"
+			loader: "url-loader?limit=10000&minetype=application/font-woff&name=img/[name]-[hash:7].[ext]"
 		}, { 
 			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
 			loader: "file?hash=sha512&digest=hex&name=img/[name]-[hash:7].[ext]"
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -20,27 +20,26 @@ module.exports = {
 			filename: 'index.html'
 		}),
 		new ExtractTextPlugin('app/[name]-[hash:7].min.css'),
-		new webpack.optimize.UglifyJsPlugin({
-			compressor: {
-				warnings: false,
-				screw_ie8: true
-			}
-		}),
+
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
 		})
 	],
 	module: {
 		loaders: [{
-			test: /\.js?$/,
+			loader: 'babel-loader',
 			exclude: /node_modules/,
-			loader: 'babel'
+			test: /\.js$/,
+			query: {
+				plugins: ['transform-runtime'],
+				presets: ['es2015', 'react']
+			}
 		}, {
 			test: /\.json?$/,
 			loader: 'json'
 		}, { 
 			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-			loader: "url-loader?limit=10000&minetype=application/font-woff"
+			loader: "url-loader?limit=10000&minetype=application/font-woff&name=img/[name]-[hash:7].[ext]"
 		}, { 
 			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
 			loader: "file?hash=sha512&digest=hex&name=img/[name]-[hash:7].[ext]"
