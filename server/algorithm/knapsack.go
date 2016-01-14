a51b51ba9c99fc5d857359742826d8eea69aa6cb
Juan B. Rodriguez <jbrodriguez@gmail.com>
2016-01-14 09:28:35 -0500

    (back) Calculate reserved space based on user selections

--- server/algorithm/knapsack.go
+++ server/algorithm/knapsack.go
@@ -17,11 +17,35 @@ type Knapsack struct {
 	buffer int64
 }
 
-func NewKnapsack(disk *model.Disk, items []*model.Item, reserved int64) *Knapsack {
+func max(x, y int64) int64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func NewKnapsack(disk *model.Disk, items []*model.Item, amount int64, unit string, floor int64) *Knapsack {
 	p := new(Knapsack)
 	p.disk = disk
 	p.list = items
-	p.buffer = reserved
+
+	var reserved int64
+	switch unit {
+	case "%":
+		fcalc := disk.Size * amount / 100
+		reserved = int64(fcalc)
+		break
+	case "Mb":
+		reserved = amount * 1000 * 1000
+		break
+	case "Gb":
+		reserved = amount * 1000 * 1000 * 1000
+		break
+	default:
+		reserved = floor
+	}
+
+	p.buffer = max(floor, reserved)
 	return p
 }
 
