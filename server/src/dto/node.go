a26f5a99c3eeaf86be955cc464152b89173d788e
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-01-12 09:11:06 -0500

    Change node structure
    Related to #33

--- server/src/dto/node.go
+++ server/src/dto/node.go
@@ -2,12 +2,17 @@ package dto
 
 // Entry -
 type Entry struct {
-	Path  string  `json:"path"`
-	Nodes []*Node `json:"nodes"`
+	Path  string `json:"path"`
+	Nodes []Node `json:"nodes"`
 }
 
 // Node -
 type Node struct {
-	Type string `json:"type"`
-	Path string `json:"path"`
+	Label     string `json:"label"`
+	Checkbox  bool   `json:"checkbox"`
+	Collapsed bool   `json:"collapsed"`
+	Children  []Node `json:"children"`
+	Path      string `json:"path"`
 }
+
+// 	{label: 'films', checkbox: true, collapsed: true, collapsible: true, children: [{label: 'bluray'},{label: 'blurip'}]},
