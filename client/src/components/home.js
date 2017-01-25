01894bcf7b76112504ac9dad6d072be3e45d0ab8
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-01-25 10:59:29 -0500

    Lint components
    Related to #37

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,53 +1,61 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
 import TreeMenu from 'react-tree-menu'
+
+import classNames from 'classnames/bind'
 
 import ConsolePanel from './consolePanel'
 
-import { humanBytes, percentage, scramble } from '../lib/utils'
+import { humanBytes, percentage } from '../lib/utils'
 
 import styles from '../styles/core.scss'
-import classNames from 'classnames/bind'
 
 require('./tree-view.css')
 
-let cx = classNames.bind(styles)
+const cx = classNames.bind(styles)
+
+const propTypes = {
+	store: React.PropTypes.arrayOf(React.PropTypes.any).isRequired,
+	actions: React.PropTypes.objectOf(React.PropTypes.func).isRequired,
+	treeCollapsed: React.PropTypes.bool.isRequired,
+}
 
 export default class Home extends Component {
-	// componentWillMount() {
-	// 	let { store, history } = this.props
-	// 	if (store.state.config.folders.length === 0) {
-	// 		history.pushState(null, '/settings')
-	// 	}
-	// }
+	onCollapse = (node) => {
+		// console.log(`collapse-node-${JSON.stringify(node)}`)
+		const { treeCollapsed } = this.props.store.actions
+		treeCollapsed(node)
+	}
 
-	// componentDidMount() {
-	// 	// // let { model, history } = this.props
-	// 	// // if (!model.config) {
-	// 	// // 	history.pushState(null, '/settings')
-	// 	// // }
-	// 	// // console.log('home.didmount.props: ', this.props)
-	// 	// this.props.dispatch(C.GET_STORAGE)
-	// 	let { actions, dispatch } = this.props.store
-	// 	dispatch(actions.getStorage)
-	// }
+	onCheck = (node) => {
+		// console.log(`check-node-${JSON.stringify(node)}`)
+		const { treeChecked } = this.props.store.actions
+		treeChecked(node)
+	}
+
+	checkFrom = path => () => {
+		const { checkFrom } = this.props.store.actions
+		checkFrom(path)
+	}
+
+	checkTo = path => (e) => {
+		const { state, actions: { checkTo } } = this.props.store
+
+		if (state.fromDisk[path]) {
+			e.preventDefault()
+			return
+		}
+
+		checkTo(path)
+	}
 
 	render() {
-		let { state, actions } = this.props.store
-		// let { dispatch, model } = this.props
-
-		// const data = [
-		// 	{label: 'films', checkbox: true, collapsed: true, collapsible: true, children: [{label: 'bluray'},{label: 'blurip'}]},
-		// 	{label: 'tvshows', checkbox: true, collapsed: true, collapsible: true, children: [{label: 'Loading ...'}]},
-		// 	{label: 'storage', checkbox: true, collapsed: true, collapsible: true, children: [{label: 'Loading ...'}]},
-		// 	{label: 'backup', checkbox: true, collapsed: true, collapsible: true, children: [{label: 'Loading ...'}]}
-		// ]
+		const { state } = this.props.store
 
 		if (!state.unraid) {
 			return null
 		}
 
-		if (state.unraid.condition.state !== "STARTED") {
+		if (state.unraid.condition.state !== 'STARTED') {
 			return (
 				<section className={cx('row', 'bottom-spacer-half')}>
 					<div className={cx('col-xs-12')}>
@@ -57,14 +65,12 @@ export default class Home extends Component {
 			)
 		}
 
-		// <span style="width: {((state.unraid.condition.size-state.unraid.condition.free) / state.unraid.condition.size )}"></span>
-
 		let consolePanel = null
 		if (state.lines.length !== 0) {
 			consolePanel = (
 				<section className={cx('row', 'bottom-spacer-half')}>
 					<div className={cx('col-xs-12')}>
-						<ConsolePanel lines={state.lines} style={'console-feedback'} />
+						<ConsolePanel lines={state.lines} styleClass={'console-feedback'} />
 					</div>
 				</section>
 			)
@@ -72,9 +78,9 @@ export default class Home extends Component {
 
 					// {percentage(disk.free/disk.size)}
 
-		let rows = 	state.unraid.disks.map( (disk, i) => {
-			let diskChanged = cx({
-				'label': disk.newFree !== disk.free,
+		const rows = 	state.unraid.disks.map((disk, i) => {
+			const diskChanged = cx({
+				label: disk.newFree !== disk.free,
 				'label-success': disk.newFree !== disk.free && state.fromDisk[disk.path],
 			})
 
@@ -83,35 +89,35 @@ export default class Home extends Component {
 			// console.log("disk.name.length: ", disk.name.length)
 
 			// let serial = scramble(disk.serial)
-			if (disk.type === "Cache" && disk.name.length > 5) {
+			if (disk.type === 'Cache' && disk.name.length > 5) {
 				return (
-					<tr key={i}>
+					<tr key={disk.id}>
 						<td>{disk.name}</td>
 						<td>{disk.fsType}</td>
 						<td colSpan="7">{disk.serial} ({disk.device})</td>
 					</tr>
 				)
-			} else {
+			} else { // eslint-disable-line
 				// lines initially contains the disk row, which includes the
 				// checkbox indicating it's either the from disk or a to disk
-				let lines = [
-					<tr key={i}>
+				const lines = [
+					<tr key={disk.id}>
 						<td>{disk.name}</td>
 						<td>{disk.fsType}</td>
 						<td>{disk.serial} ({disk.device})</td>
-						<td><input type="checkbox" checked={state.fromDisk[disk.path]} onChange={this._checkFrom(disk.path)} /></td>
-						<td><input type="checkbox" checked={state.toDisk[disk.path]} onChange={this._checkTo(disk.path)} /></td>
+						<td><input type="checkbox" checked={state.fromDisk[disk.path]} onChange={this.checkFrom(disk.path)} /></td>
+						<td><input type="checkbox" checked={state.toDisk[disk.path]} onChange={this.checkTo(disk.path)} /></td>
 						<td>{humanBytes(disk.size)}</td>
 						<td>{humanBytes(disk.free)}</td>
 						<td>
 							<div className={cx('progress')}>
-								<span style={{width: percent}}></span>
+								<span style={{ width: percent }} />
 							</div>
 						</td>
 						<td>
 							<span className={diskChanged}>{humanBytes(disk.newFree)}</span>
 						</td>
-					</tr>
+					</tr>,
 				]
 
 				// if it's the source disk, let's add a second row, with the
@@ -121,25 +127,24 @@ export default class Home extends Component {
 					lines.push(
 						<tr key={key}>
 							<td colSpan="3">
-							<b>Select folders/files to move</b><br/>
-							<TreeMenu
-								expandIconClass="fa fa-chevron-right"
-						        collapseIconClass="fa fa-chevron-down"
-								onTreeNodeClick={this.onClick}
-								onTreeNodeCollapseChange={this.onCollapse}
-								onTreeNodeCheckChange={this.onCheck}
-								collapsible={true}
-								collapsed={false}
-								data={state.tree.items}
-							/>
+								<b>Select folders/files to move</b><br />
+								<TreeMenu
+									expandIconClass="fa fa-chevron-right"
+									collapseIconClass="fa fa-chevron-down"
+									onTreeNodeCollapseChange={this.onCollapse}
+									onTreeNodeCheckChange={this.onCheck}
+									collapsible
+									collapsed={false}
+									data={state.tree.items}
+								/>
 							</td>
 							<td colSpan="6" className={cx('topAlign')}>
-								<b>Currently selected</b><br/>
+								<b>Currently selected</b><br />
 								<ul>
-								{ Object.keys(state.tree.chosen).map( chosen => <li>- {chosen}</li> )}
+									{ Object.keys(state.tree.chosen).map(chosen => <li>- {chosen}</li>) }
 								</ul>
 							</td>
-						</tr>
+						</tr>,
 					)
 				}
 
@@ -147,21 +152,21 @@ export default class Home extends Component {
 			}
 		})
 
-		let grid = (
+		const grid = (
 			<section className={cx('row', 'bottom-spacer-half')}>
 				<div className={cx('col-xs-12')}>
 					<table>
 						<thead>
 							<tr>
-								<th style={{width: '70px'}}>DISK</th>
-								<th style={{width: '75px'}}>TYPE</th>
+								<th style={{ width: '70px' }}>DISK</th>
+								<th style={{ width: '75px' }}>TYPE</th>
 								<th>SERIAL</th>
-								<th style={{width: '50px'}}>FROM </th>
-								<th style={{width: '50px'}}>TO</th>
-								<th style={{width: '100px'}}>SIZE</th>
-								<th style={{width: '85px'}}>FREE</th>
-								<th style={{width: '100px'}}>FILL</th>
-								<th style={{width: '100px'}}>PLAN</th>
+								<th style={{ width: '50px' }}>FROM </th>
+								<th style={{ width: '50px' }}>TO</th>
+								<th style={{ width: '100px' }}>SIZE</th>
+								<th style={{ width: '85px' }}>FREE</th>
+								<th style={{ width: '100px' }}>FILL</th>
+								<th style={{ width: '100px' }}>PLAN</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -173,8 +178,7 @@ export default class Home extends Component {
 								<th>{humanBytes(state.unraid.condition.size)}</th>
 								<th>{humanBytes(state.unraid.condition.free)}</th>
 								<th>
-									<div className={cx('progress')}>
-									</div>
+									<div className={cx('progress')} />
 								</th>
 								<th>{humanBytes(state.unraid.condition.free)}</th>
 							</tr>
@@ -187,41 +191,9 @@ export default class Home extends Component {
 		return (
 			<div>
 				{ consolePanel }
-
 				{ grid }
 			</div>
 		)
 	}
-
-	_checkFrom = (path) => (e) => {
-		let { checkFrom } = this.props.store.actions
-		checkFrom(path)
-	}
-
-	_checkTo = (path) => (e) => {
-		let { state, actions: {checkTo} } = this.props.store
-
-		if (state.fromDisk[path]) {
-			e.preventDefault()
-			return
-		}
-
-		checkTo(path)
-	}
-
-	onClick = (node) => {
-		// console.log(`click-node-${JSON.stringify(node)}`)
-	}
-
-	onCollapse = (node) => {
-		// console.log(`collapse-node-${JSON.stringify(node)}`)
-		let { treeCollapsed } = this.props.store.actions
-		treeCollapsed(node)
-	}
-
-	onCheck = (node) => {
-		// console.log(`check-node-${JSON.stringify(node)}`)
-		let { treeChecked } = this.props.store.actions
-		treeChecked(node)
-	}
 }
+Home.propTypes = propTypes
--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
-import 'font-awesome-webpack'
 
-import styles from '../styles/core.scss'
+import 'font-awesome-webpack' // eslint-disable-line
 import classNames from 'classnames/bind'
 
-let cx = classNames.bind(styles)
+import styles from '../styles/core.scss'
+
+const cx = classNames.bind(styles)
+
+const propTypes = {
+	store: React.PropTypes.arrayOf(React.PropTypes.any).isRequired,
+	actions: React.PropTypes.objectOf(React.PropTypes.func).isRequired,
+}
 
 export default class Settings extends Component {
 	constructor(props) {
@@ -17,17 +22,10 @@ export default class Settings extends Component {
 			rsyncFlags: props.store.state.config.rsyncFlags,
 		}
 	}
-	// componentDidMount() {
-	// 	let { actions, dispatch } = this.props.store
-	// 	dispatch(actions.getConfig)
-	// }
-
-						// <p>Set up the minimum amount of space that should be left free on each disk, after moving folders.</p>
-
 
 	componentWillReceiveProps(next) {
 		const { reservedAmount, reservedUnit, rsyncFlags } = next.store.state.config
-		if (reservedAmount !== this.state.reservedAmount || reservedUnit !== this.state.reservedUnit ||  rsyncFlags !== this.state.rsyncFlags) {
+		if (reservedAmount !== this.state.reservedAmount || reservedUnit !== this.state.reservedUnit || rsyncFlags !== this.state.rsyncFlags) {
 			this.setState({
 				reservedUnit,
 				reservedAmount,
@@ -36,24 +34,63 @@ export default class Settings extends Component {
 		}
 	}
 
+	setNotifyCalc = notify => () => {
+		const { setNotifyCalc } = this.props.store.actions
+		setNotifyCalc(notify)
+	}
+
+	setNotifyMove = notify => () => {
+		const { setNotifyMove } = this.props.store.actions
+		setNotifyMove(notify)
+	}
+
+	setReservedAmount = (e) => {
+		this.setState({
+			reservedAmount: e.target.value,
+		})
+	}
+
+	setReservedUnit = (e) => {
+		this.setState({
+			reservedUnit: e.target.value,
+		})
+	}
+
+	setReservedSpace = () => {
+		const { setReservedSpace } = this.props.store.actions
+		setReservedSpace(this.state.reservedAmount, this.state.reservedUnit)
+	}
+
+	_onChangeRsyncFlags = (e) => {
+		this.setState({
+			rsyncFlags: e.target.value.split(' '),
+		})
+	}
+
+	_setRsyncFlags = () => {
+		const { setRsyncFlags } = this.props.store.actions
+		const flags = this.state.rsyncFlags.join(' ')
+		setRsyncFlags(flags.trim().split(' '))
+	}
+
+	_setRsyncDefault = () => {
+		const { setRsyncFlags } = this.props.store.actions
+		setRsyncFlags(['-avPRX'])
+	}
+
 	render() {
-		// let { dispatch, state } = this.props
-		// console.log('settings.render: ', this.props.store)
-		let { state, actions } = this.props.store
+		const { state, actions } = this.props.store
 
 		if (!state.config) {
 			return null
 		}
 
-		// console.log('state.unraid: ', state.unraid)
 		if (!state.unraid) {
-			// dispatch(actions.getStorage)
 			return null
 		}
 
-		const stateOk = state.unraid && state.unraid.condition.state === "STARTED"
+		const stateOk = state.unraid && state.unraid.condition.state === 'STARTED'
 		if (!stateOk) {
-			// console.log('stateOk: ', stateOk)
 			return (
 				<section className={cx('row', 'bottom-spacer-half')}>
 					<div className={cx('col-xs-12')}>
@@ -73,153 +110,99 @@ export default class Settings extends Component {
 			)
 		}
 
-		let flags = this.state.rsyncFlags.join(' ')
+		const flags = this.state.rsyncFlags.join(' ')
 
 		return (
 			<div>
 
-			<section className={cx('row', 'bottom-spacer-2x')}>
-				<div className={cx('col-xs-12')}>
-					<div>
-						<h3>SET UP NOTIFICATIONS</h3>
+				<section className={cx('row', 'bottom-spacer-2x')}>
+					<div className={cx('col-xs-12')}>
+						<div>
+							<h3>SET UP NOTIFICATIONS</h3>
 
-						<p>Notifications rely on unRAID's notifications settings, so you need to set up unRAID first, in order to receive notifications from unBALANCE.</p>
+							<p>Notifications rely on unRAID&apos;s notifications settings, so you need to set up unRAID first, in order to receive notifications from unBALANCE.</p>
 
-						<span> Calculate: </span>
-						<input id="calc0" className={cx('lspacer')} type="radio" name="calc" checked={state.config.notifyCalc === 0} onChange={this._setNotifyCalc(0)} />
-						<label id="calc0" >No Notifications</label>
+							<span> Calculate: </span>
+							<input id="calc0" className={cx('lspacer')} type="radio" name="calc" checked={state.config.notifyCalc === 0} onChange={this.setNotifyCalc(0)} />
+							<label htmlFor="calc0" >No Notifications</label>
 
-						<input id="calc1" className={cx('lspacer')} type="radio" name="calc" checked={state.config.notifyCalc === 1} onChange={this._setNotifyCalc(1)} />
-						<label id="calc1" >Basic</label>
+							<input id="calc1" className={cx('lspacer')} type="radio" name="calc" checked={state.config.notifyCalc === 1} onChange={this.setNotifyCalc(1)} />
+							<label htmlFor="calc1" >Basic</label>
 
-						<input id="calc2" className={cx('lspacer')} type="radio" name="calc" checked={state.config.notifyCalc === 2} onChange={this._setNotifyCalc(2)} />
-						<label id="calc2" >Detailed</label>
+							<input id="calc2" className={cx('lspacer')} type="radio" name="calc" checked={state.config.notifyCalc === 2} onChange={this.setNotifyCalc(2)} />
+							<label htmlFor="calc2" >Detailed</label>
 
-						<br />
+							<br />
 
-						<span> Move: </span>
-						<input id="move0" className={cx('lspacer')} type="radio" name="move" checked={state.config.notifyMove === 0} onChange={this._setNotifyMove(0)} />
-						<label id="move0">No Notifications</label>
+							<span> Move: </span>
+							<input id="move0" className={cx('lspacer')} type="radio" name="move" checked={state.config.notifyMove === 0} onChange={this.setNotifyMove(0)} />
+							<label htmlFor="move0">No Notifications</label>
 
-						<input id="move0" className={cx('lspacer')} type="radio" name="move" checked={state.config.notifyMove === 1} onChange={this._setNotifyMove(1)} />
-						<label id="move0">Basic</label>
+							<input id="move0" className={cx('lspacer')} type="radio" name="move" checked={state.config.notifyMove === 1} onChange={this.setNotifyMove(1)} />
+							<label htmlFor="move0">Basic</label>
 
-						<input id="move0" className={cx('lspacer')} type="radio" name="move" checked={state.config.notifyMove === 2} onChange={this._setNotifyMove(2)} />
-						<label id="move0">Detailed</label>
+							<input id="move0" className={cx('lspacer')} type="radio" name="move" checked={state.config.notifyMove === 2} onChange={this.setNotifyMove(2)} />
+							<label htmlFor="move0">Detailed</label>
+						</div>
 					</div>
-				</div>
-			</section>
+				</section>
 
-			<section className={cx('row', 'bottom-spacer-2x')}>
-				<div className={cx('col-xs-12')}>
-					<div>
-						<h3>RESERVED SPACE</h3>
+				<section className={cx('row', 'bottom-spacer-2x')}>
+					<div className={cx('col-xs-12')}>
+						<div>
+							<h3>RESERVED SPACE</h3>
 
-						<p>unBALANCE uses the threshold defined here as the minimum free space that should be kept available in a target disk, when calculating how much the disk can be filled.</p>
-						<p>This threshold cannot be less than 450Mb (hard limit set by this app).</p>
+							<p>unBALANCE uses the threshold defined here as the minimum free space that should be kept available in a target disk, when calculating how much the disk can be filled.</p>
+							<p>This threshold cannot be less than 450Mb (hard limit set by this app).</p>
 
-						<div className={cx('row')}>
-							<div className={cx('col-xs-2')}>
-								<div className={cx('addon')}>
-									<input className={cx('addon-field')} type="number" value={this.state.reservedAmount} onChange={this._setReservedAmount} />
-									<select className={cx('addon-item')} name="unit" value={this.state.reservedUnit} onChange={this._setReservedUnit}>
-										<option value="%">%</option>
-										<option value="Mb">Mb</option>
-										<option value="Gb">Gb</option>
-									</select>
+							<div className={cx('row')}>
+								<div className={cx('col-xs-2')}>
+									<div className={cx('addon')}>
+										<input className={cx('addon-field')} type="number" value={this.state.reservedAmount} onChange={this.setReservedAmount} />
+										<select className={cx('addon-item')} name="unit" value={this.state.reservedUnit} onChange={this.setReservedUnit}>
+											<option value="%">%</option>
+											<option value="Mb">Mb</option>
+											<option value="Gb">Gb</option>
+										</select>
+									</div>
 								</div>
-							</div>
-							<div className={cx('col-xs-1')}>
-								<button className={cx('btn', 'btn-primary')} onClick={this._setReservedSpace}>Apply</button>
+								<div className={cx('col-xs-1')}>
+									<button className={cx('btn', 'btn-primary')} onClick={this.setReservedSpace}>Apply</button>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</section>
+				</section>
 
-			<section className={cx('row', 'bottom-spacer-2x')}>
-				<div className={cx('col-xs-12')}>
-					<div>
-						<h3>CUSTOM RSYNC FLAGS</h3>
+				<section className={cx('row', 'bottom-spacer-2x')}>
+					<div className={cx('col-xs-12')}>
+						<div>
+							<h3>CUSTOM RSYNC FLAGS</h3>
 
-						<p>Internally unBALANCE uses rsync to transfer files across disks.</p>
-						<p>By default, rsync is invoked with <b>-avRX --partial</b> flags.</p>
-						<p>Here you can set custom flags to override the default ones, except for the dry run flag which will be automatically added, if needed.</p>
-						<p>It's strongly recommended to keep the -R flag, for optimal operation.</p>
-						<p>Be careful with the flags you choose, since it can drastically alter the expected behaviour of rsync under unBALANCE.</p>
+							<p>Internally unBALANCE uses rsync to transfer files across disks.</p>
+							<p>By default, rsync is invoked with <b>-avRX --partial</b> flags.</p>
+							<p>Here you can set custom flags to override the default ones, except for the dry run flag which will be automatically added, if needed.</p>
+							<p>It&apos;s strongly recommended to keep the -R flag, for optimal operation.</p>
+							<p>Be careful with the flags you choose, since it can drastically alter the expected behaviour of rsync under unBALANCE.</p>
 
-						<div className={cx('row')}>
-							<div className={cx('col-xs-2')}>
-								<div className={cx('addon')}>
-									<input className={cx('addon-field')} type="string" value={flags} onChange={this._onChangeRsyncFlags} />
+							<div className={cx('row')}>
+								<div className={cx('col-xs-2')}>
+									<div className={cx('addon')}>
+										<input className={cx('addon-field')} type="string" value={flags} onChange={this.onChangeRsyncFlags} />
+									</div>
 								</div>
-							</div>
-							<div className={cx('col-xs-4')}>
-								<button className={cx('btn', 'btn-primary')} onClick={this._setRsyncFlags}>Apply</button>
-								&nbsp;
-								<button className={cx('btn', 'btn-primary')} onClick={this._setRsyncDefault}>Reset to default</button>
+								<div className={cx('col-xs-4')}>
+									<button className={cx('btn', 'btn-primary')} onClick={this.setRsyncFlags}>Apply</button>
+									&nbsp;
+									<button className={cx('btn', 'btn-primary')} onClick={this.setRsyncDefault}>Reset to default</button>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</section>
+				</section>
 
 			</div>
 		)
 	}
-
-	// _addFolder(dispatch, e) {
-	// 	console.log('key - value: ', e.key, e.target.value)
-	// 	if (e.key !== "Enter") {
-	// 		return
-	// 	}
-
-	// 	e.preventDefault()
-
-	// 	dispatch(C.ADD_FOLDER, e.target.value)
-	// }
-
-	_setNotifyCalc = (notify) => (e) => {
-		const { setNotifyCalc } = this.props.store.actions
-		setNotifyCalc(notify)
-	}
-
-	_setNotifyMove = (notify) => (e) => {
-		const { setNotifyMove } = this.props.store.actions
-		setNotifyMove(notify)
-	}
-
-	_setReservedAmount = (e) => {
-		this.setState({
-			reservedAmount: e.target.value
-		})
-	}
-
-	_setReservedUnit = (e) => {
-		this.setState({
-			reservedUnit: e.target.value
-		})
-	}
-
-	_setReservedSpace = (e) => {
-		const { setReservedSpace } = this.props.store.actions
-		setReservedSpace(this.state.reservedAmount, this.state.reservedUnit)
-	}
-
-	_onChangeRsyncFlags = (e) => {
-		this.setState({
-			rsyncFlags: e.target.value.split(' ')
-		})
-	}
-
-	_setRsyncFlags = () => {
-		const { setRsyncFlags } = this.props.store.actions
-		const flags = this.state.rsyncFlags.join(' ')
-		setRsyncFlags(flags.trim().split(' '))
-	}
-
-	_setRsyncDefault = (e) => {
-		const { setRsyncFlags } = this.props.store.actions
-		setRsyncFlags(['-avPRX'])
-	}
 }
+Settings.propTypes = propTypes
