1fb3f0490ba0ebbee875b916e860dc57222ca098
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-01-25 11:48:53 -0500

    Lint lib
    Related to #37

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,92 +2,92 @@ import fetch from 'isomorphic-fetch'
 
 export default class Api {
 	constructor() {
-		this.hostr = 'http://' + document.location.host + '/api/v1'
+		this.hostr = `http://${document.location.host}/api/v1`
 	}
 
 	getConfig() {
 		// console.log('this.hostr', this.hostr)
-		return fetch(this.hostr + '/config')
+		return fetch(`${this.hostr}/config`)
 			.then(resp => resp.json())
 	}
 
 	setNotifyCalc(notify) {
-		return fetch(this.hostr + '/config/notifyCalc', {
+		return fetch(`${this.hostr}/config/notifyCalc`, {
 			method: 'PUT',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({payload: notify})
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({ payload: notify }),
 		})
 		.then(resp => resp.json())
 	}
 
 	setNotifyMove(notify) {
-		return fetch(this.hostr + '/config/notifyMove', {
+		return fetch(`${this.hostr}/config/notifyMove`, {
 			method: 'PUT',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({payload: notify})
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({ payload: notify }),
 		})
 		.then(resp => resp.json())
 	}
 
 	setReservedSpace(amount, unit) {
-		return fetch(this.hostr + '/config/reservedSpace', {
+		return fetch(`${this.hostr}/config/reservedSpace`, {
 			method: 'PUT',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({payload: JSON.stringify({amount, unit})})
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({ payload: JSON.stringify({ amount, unit }) }),
 		})
 		.then(resp => resp.json())
 	}
 
 	addFolder(folder) {
 		// console.log('sending: ', folder)
-		return fetch(this.hostr + '/config/folder', {
+		return fetch(`${this.hostr}/config/folder`, {
 			method: 'PUT',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({payload: folder})
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({ payload: folder }),
 		})
 		.then(resp => resp.json())
 	}
 
 	deleteFolder(folder) {
 		// console.log('sending: ', folder)
-		return fetch(this.hostr + '/config/folder', {
+		return fetch(`${this.hostr}/config/folder`, {
 			method: 'DELETE',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({payload: folder})
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({ payload: folder }),
 		})
 		.then(resp => resp.json())
 	}
 
 	getStorage() {
 		// console.log('this.hostr', this.hostr)
-		return fetch(this.hostr + '/storage')
+		return fetch(`${this.hostr}/storage`)
 			.then(resp => resp.json())
 	}
 
 	getTree(path) {
-		return fetch(this.hostr + '/tree', {
+		return fetch(`${this.hostr}/tree`, {
 			method: 'POST',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({payload: path})
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({ payload: path }),
 		})
 		.then(resp => resp.json())
 	}
 
 	toggleDryRun(dryRun) {
-		return fetch(this.hostr + '/config/dryRun', {
+		return fetch(`${this.hostr}/config/dryRun`, {
 			method: 'PUT',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({payload: dryRun})
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({ payload: dryRun }),
 		})
 		.then(resp => resp.json())
 	}
 
 	setRsyncFlags(rsyncFlags) {
-		return fetch(this.hostr + '/config/rsyncFlags', {
+		return fetch(`${this.hostr}/config/rsyncFlags`, {
 			method: 'PUT',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({payload: JSON.stringify({rsyncFlags})})
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({ payload: JSON.stringify({ rsyncFlags }) }),
 		})
-		.then(resp => resp.json())		
+		.then(resp => resp.json())
 	}
 }
--- client/src/lib/wsapi.js
+++ client/src/lib/wsapi.js
@@ -1,31 +1,28 @@
 export default class WebSocketApi {
 	constructor() {
-		const hostw = "ws://" + document.location.host + "/skt"
+		const hostw = `ws://${document.location.host}/skt`
 
 		this.skt = new WebSocket(hostw)
 
-		this.skt.onopen = function() {
-		    console.log("Connection opened")
+		this.skt.onopen = () => {
+			console.log('Connection opened')
 		}
 
-		this.skt.onclose = function() {
-		    console.log("Connection is closed...")
-		}		
+		this.skt.onclose = () => {
+			console.log('Connection is closed...')
+		}
 	}
 
 	receive(fn) {
 		this.skt.onmessage = fn
 	}
 
-	send({topic, payload}) {
-		// console.log('topic: ', topic)
+	send({ topic, payload }) {
 		const packet = {
 			topic,
-			payload: JSON.stringify(payload)
+			payload: JSON.stringify(payload),
 		}
 
-		// console.log('packet: ', packet)
-
-		this.skt.send(JSON.stringify(packet))			
-	} 
+		this.skt.send(JSON.stringify(packet))
+	}
 }
