e245d60dde5289e61858c62ebf60164901b93ae1
Juan B. Rodriguez <jbrodriguez@gmail.com>
2017-06-21 15:47:40 -0500

    Implement operation state
    This operation state holds information about the current operation parameters, so that it can be used across operations.
    
    Related to #46

---
+++ server/src/model/command.go
@@ -0,0 +1,8 @@
+package model
+
+type Command struct {
+	Src  string
+	Dst  string
+	Path string
+	Size int64
+}
---
+++ server/src/model/operation.go
@@ -0,0 +1,31 @@
+package model
+
+import (
+	"time"
+)
+
+const (
+	StateIdle     = 0
+	StateCalc     = 1
+	StateMove     = 2
+	StateCopy     = 3
+	StateValidate = 4
+)
+
+type Operation struct {
+	PrevState             uint64
+	OpState               uint64
+	SourceDiskName        string
+	RsyncFlags            []string
+	RsyncStrFlags         string
+	Commands              []Command
+	BytesToTransfer       int64
+	BytesTransferred      int64
+	Started               time.Time
+	Finished              time.Time
+	FoldersNotTransferred []string
+	OwnerIssue            int64
+	GroupIssue            int64
+	FolderIssue           int64
+	FileIssue             int64
+}
