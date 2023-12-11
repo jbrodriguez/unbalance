77cbce8ed351fc4ca8c430eb99de3fc43e142d8c
Juan B. Rodriguez <jbrodriguez@gmail.com>
2023-12-11 08:32:13 -0500

    chore: modify dashboard ui

--- ui/src/shared/transfer/dashboard.tsx
+++ ui/src/shared/transfer/dashboard.tsx
@@ -3,64 +3,54 @@ import React from 'react';
 export const Dashboard: React.FC = () => {
   return (
     <div className="grid grid-cols-5 gap-6 text-blue-600 ">
-      <div className="border border-stroke dark:border-gray-800 border-slate-300 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div className="flex items-end justify-between px-4">
-          <div>
-            <h1 className="text-3xl">52.33</h1>
-            <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
-              Completed
-            </span>
-          </div>
-          <span className="text-sm font-medium">%</span>
+      <div className="border border-stroke dark:border-gray-800 border-slate-300 px-4 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="flex justify-between items-center">
+          <span className="text-3xl">52.33</span>
+          <span>%</span>
         </div>
+        <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
+          Completed
+        </span>
       </div>
 
-      <div className="border border-stroke dark:border-gray-800 border-slate-300 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div className="flex items-end justify-between px-4">
-          <div>
-            <h1 className="text-3xl">42.38</h1>
-            <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
-              Speed
-            </span>
-          </div>
-          <span className="text-sm font-medium">MB/s</span>
+      <div className="border border-stroke dark:border-gray-800 border-slate-300 px-4 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="flex justify-between items-center">
+          <span className="text-3xl">42.38</span>
+          <span>MB/s</span>
         </div>
+        <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
+          Speed
+        </span>
       </div>
 
-      <div className="border border-stroke dark:border-gray-800 border-slate-300 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div className="flex items-end justify-between px-4">
-          <div>
-            <h1 className="text-3xl">1.39</h1>
-            <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
-              Transferred
-            </span>
-          </div>
-          <span className="text-sm font-medium">GB</span>
+      <div className="border border-stroke dark:border-gray-800 border-slate-300 px-4 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="flex justify-between items-center">
+          <span className="text-3xl">1.39</span>
+          <span>GB</span>
         </div>
+        <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
+          Transferred
+        </span>
       </div>
 
-      <div className="border border-stroke dark:border-gray-800 border-slate-300 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div className="flex items-end justify-between px-4">
-          <div>
-            <h1 className="text-3xl">2.66</h1>
-            <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
-              Total
-            </span>
-          </div>
-          <span className="text-sm font-medium">GB</span>
+      <div className="border border-stroke dark:border-gray-800 border-slate-300 px-4 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="flex justify-between items-center">
+          <span className="text-3xl">2.66</span>
+          <span>GB</span>
         </div>
+        <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
+          Total
+        </span>
       </div>
 
-      <div className="border border-stroke dark:border-gray-800 border-slate-300 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div className="flex items-end justify-between px-4">
-          <div>
-            <h1 className="text-3xl">28</h1>
-            <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
-              Remaining
-            </span>
-          </div>
-          <span className="text-sm font-medium">s</span>
+      <div className="border border-stroke dark:border-gray-800 border-slate-300 px-4 py-3 shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="flex justify-between items-center">
+          <span className="text-3xl">28</span>
+          <span>sec</span>
         </div>
+        <span className="text-sm font-medium dark:text-slate-600 text-slate-600">
+          Remaining
+        </span>
       </div>
     </div>
   );
