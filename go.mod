d90be66dc3069726a350646347a0f69cc8c5e86e
Juan B. Rodriguez <jbrodriguez@gmail.com>
2023-12-14 15:51:14 -0500

    feat: implement tree view using rc-tree

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
--- ui/src/flows/scatter/select/filesystem.tsx
+++ ui/src/flows/scatter/select/filesystem.tsx
@@ -1,167 +1,78 @@
 import React from 'react';
-import CheckboxTree from 'react-checkbox-tree';
-// import IconAccountBox from 'virtual:vite-icons/ic/baseline-check-box/';
-// import { Icon } from '~/shared/icons/icon';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCheckSquare,
-  faSquareMinus,
-  faChevronRight,
-  faChevronDown,
-  faPlusSquare,
-  faMinusSquare,
-  faFolder,
-  faFolderOpen,
-  faFile,
-} from '@fortawesome/free-solid-svg-icons';
-import { faSquare } from '@fortawesome/free-regular-svg-icons';
 
-import 'react-checkbox-tree/lib/react-checkbox-tree.css';
-
-const nodes = [
-  {
-    value: 'Documents',
-    label: 'Documents',
-    children: [
-      {
-        value: 'Employee Evaluations.zip',
-        label: 'Employee Evaluations.zip',
-        // icon: <i className="far fa-file-archive" />,
-      },
-      {
-        value: 'Expense Report.pdf',
-        label: 'Expense Report.pdf',
-        // icon: <i className="far fa-file-pdf" />,
-      },
-      {
-        value: 'notes.txt',
-        label: 'notes.txt',
-        // icon: <i className="far fa-file-alt" />,
-      },
-    ],
-  },
-  {
-    value: 'Photos',
-    label: 'Photos',
-    children: [
-      {
-        value: 'nyan-cat.gif',
-        label: 'nyan-cat.gif',
-        // icon: <i className="far fa-file-image" />,
-      },
-      {
-        value: 'SpaceX Falcon9 liftoff.jpg',
-        label: 'SpaceX Falcon9 liftoff.jpg',
-        // icon: <i className="far fa-file-image" />,
-      },
-    ],
-  },
-];
+import Tree, { BasicDataNode } from 'rc-tree';
+import 'rc-tree/assets/index.css';
 
 interface Props {
   height?: number;
 }
 
-export const FileSystem: React.FC<Props> = ({ height = 0 }) => {
-  const [checked, setChecked] = React.useState<string[]>([]);
-  const [expanded, setExpanded] = React.useState<string[]>(['Documents']);
+export type EventDataNode<BasicDataNode> = {
+  key: React.Key;
+  expanded: boolean;
+  selected: boolean;
+  checked: boolean;
+  loaded: boolean;
+  loading: boolean;
+  halfChecked: boolean;
+  dragOver: boolean;
+  dragOverGapTop: boolean;
+  dragOverGapBottom: boolean;
+  pos: string;
+  active: boolean;
+} & BasicDataNode;
 
-  const onCheck = (value: string[]) => {
-    console.log('onCheck ', value);
-    setChecked(value);
+const initialData = [{ title: 'mnt', key: '/mnt', isLeaf: false }];
+
+type CheckedKeysType =
+  | { checked: React.Key[]; halfChecked: React.Key[] }
+  | React.Key[];
+
+export function FileSystem({ height }: Props) {
+  const [data, setData] = React.useState<BasicDataNode[]>(initialData);
+  const [checked, setChecked] = React.useState<CheckedKeysType>([]);
+
+  const onSelect = (info: unknown) => {
+    console.log('selected', info);
   };
 
-  const onExpand = (value: string[]) => {
-    console.log('onExpand ', value);
-    setExpanded(value);
+  const onCheck = (checkedKeys: CheckedKeysType) => {
+    console.log(checkedKeys);
+    setChecked(checkedKeys);
+  };
+
+  const getTree = async (treeNode: EventDataNode<BasicDataNode>) => {
+    // @ts-expect-error ts-migrate(2531) FIXME: Object is possibly 'null'.
+    treeNode.children = [{ title: 'films', key: '/mnt/films', isLeaf: false }];
+    return treeNode;
+  };
+
+  const onLoadData = async (treeNode: EventDataNode<BasicDataNode>) => {
+    console.log('load data... ', treeNode);
+
+    const loaded = await getTree(treeNode);
+    const treeData = [...data];
+    // @ts-expect-error ts-migrate(2531) FIXME: Object is possibly 'null'.
+    const index = treeData.findIndex((e) => e.key === treeNode.key);
+    if (index > -1) {
+      treeData[index] = loaded;
+    }
+
+    setData(treeData);
   };
 
   return (
     <div className="flex flex-1 bg-neutral-200 dark:bg-gray-950">
       <div className={`overflow-y-auto`} style={{ height: `${height}px` }}>
-        <FontAwesomeIcon
-          className="rct-icon rct-icon-check"
-          icon="check-square"
-        />
-
-        <CheckboxTree
-          checked={checked}
-          expanded={expanded}
-          nodes={nodes}
+        <Tree
+          onSelect={onSelect}
+          checkable
           onCheck={onCheck}
-          onExpand={onExpand}
-          icons={{
-            check: (
-              <FontAwesomeIcon
-                className="fill-red-400 text-yellow-600"
-                icon={faCheckSquare}
-              />
-            ),
-            uncheck: (
-              <FontAwesomeIcon className="text-slate-700" icon={faSquare} />
-            ),
-            halfCheck: (
-              <FontAwesomeIcon
-                className="rct-icon rct-icon-half-check"
-                icon={faSquareMinus}
-              />
-            ),
-            expandClose: (
-              <FontAwesomeIcon
-                className="rct-icon rct-icon-expand-close"
-                icon={faChevronRight}
-              />
-            ),
-            expandOpen: (
-              <FontAwesomeIcon
-                className="rct-icon rct-icon-expand-open"
-                icon={faChevronDown}
-              />
-            ),
-            expandAll: (
-              <FontAwesomeIcon className="fill-red-600" icon={faPlusSquare} />
-            ),
-            collapseAll: (
-              <FontAwesomeIcon
-                className="rct-icon rct-icon-collapse-all"
-                icon={faMinusSquare}
-              />
-            ),
-            parentClose: (
-              <FontAwesomeIcon
-                className="rct-icon rct-icon-parent-close"
-                icon={faFolder}
-              />
-            ),
-            parentOpen: (
-              <FontAwesomeIcon
-                className="rct-icon rct-icon-parent-open"
-                icon={faFolderOpen}
-              />
-            ),
-            leaf: (
-              <FontAwesomeIcon
-                className="rct-icon rct-icon-leaf-close"
-                icon={faFile}
-              />
-            ),
-          }}
-          // icons={{
-          //   check: (
-          //     <Icon name="checkbox" fill="dark:fill-slate-400 fill-slate-400" />
-          //   ),
-          //   uncheck: <Icon name="checkbox-blank" />,
-          //   halfCheck: <span className="rct-icon rct-icon-half-check" />,
-          //   expandClose: <span className="rct-icon rct-icon-expand-close" />,
-          //   expandOpen: <span className="rct-icon rct-icon-expand-open" />,
-          //   expandAll: <span className="rct-icon rct-icon-expand-all" />,
-          //   collapseAll: <span className="rct-icon rct-icon-collapse-all" />,
-          //   parentClose: <span className="rct-icon rct-icon-parent-close" />,
-          //   parentOpen: <span className="rct-icon rct-icon-parent-open" />,
-          //   leaf: <span className="rct-icon rct-icon-leaf" />,
-          // }}
+          checkedKeys={checked}
+          loadData={onLoadData}
+          treeData={data}
         />
       </div>
     </div>
   );
-};
+}
