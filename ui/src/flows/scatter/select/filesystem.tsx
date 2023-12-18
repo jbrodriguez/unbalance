feecae18c3845e2e54cb3435676810591f199163
Juan B. Rodriguez <jbrodriguez@gmail.com>
2023-12-18 12:23:14 -0500

    feat: implement width for the file system pane

--- ui/src/flows/scatter/select/filesystem.tsx
+++ ui/src/flows/scatter/select/filesystem.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import AutoSizer from 'react-virtualized-auto-sizer';
+
 import { CheckboxTree } from '~/shared/tree/checkbox-tree';
 import { Nodes, Node } from '~/types';
 import { decorateNode } from '~/shared/tree/utils';
@@ -86,74 +88,78 @@ export function FileSystem({ height }: Props) {
 
   return (
     <div className="flex flex-1 bg-neutral-200 dark:bg-gray-950">
-      <div
-        className="overflow-y-auto overflow-x-auto p-4 w-full"
-        style={{ height: `${height}px` }}
-      >
-        <CheckboxTree
-          nodes={nodes}
-          onLoad={onLoad}
-          onCheck={onCheck}
-          icons={{
-            collapseIcon: (
-              <Icon
-                name="minus"
-                size={20}
-                fill="fill-slate-500 dark:fill-gray-700"
-              />
-            ),
-            expandIcon: (
-              <Icon
-                name="plus"
-                size={20}
-                fill="fill-slate-500 dark:fill-gray-700"
-              />
-            ),
-            checkedIcon: (
-              <Icon
-                name="checked"
-                size={20}
-                fill="fill-slate-700 dark:fill-slate-200"
-              />
-            ),
-            uncheckedIcon: (
-              <Icon
-                name="unchecked"
-                size={20}
-                fill="fill-slate-700 dark:fill-slate-200"
-              />
-            ),
-            leafIcon: (
-              <Icon
-                name="file"
-                size={20}
-                fill="fill-blue-400 dark:fill-blue-700"
-              />
-            ),
-            parentIcon: (
-              <Icon
-                name="folder"
-                size={20}
-                fill="fill-orange-400 dark:fill-yellow-300"
-              />
-            ),
-            hiddenIcon: (
-              <Icon
-                name="square"
-                size={20}
-                fill="fill-neutral-200 dark:fill-gray-950"
-              />
-            ),
-            loadingIcon: (
-              <Icon
-                name="loading"
-                size={20}
-                fill="animate-spin fill-slate-700 dark:fill-slate-700"
-              />
-            ),
-          }}
-        />
-      </div>
+      <AutoSizer disableHeight>
+        {({ width }) => (
+          <div
+            className="overflow-y-auto overflow-x-auto p-4"
+            style={{ height: `${height}px`, width: `${width}px` }}
+          >
+            <CheckboxTree
+              nodes={nodes}
+              onLoad={onLoad}
+              onCheck={onCheck}
+              icons={{
+                collapseIcon: (
+                  <Icon
+                    name="minus"
+                    size={20}
+                    fill="fill-slate-500 dark:fill-gray-700"
+                  />
+                ),
+                expandIcon: (
+                  <Icon
+                    name="plus"
+                    size={20}
+                    fill="fill-slate-500 dark:fill-gray-700"
+                  />
+                ),
+                checkedIcon: (
+                  <Icon
+                    name="checked"
+                    size={20}
+                    fill="fill-slate-700 dark:fill-slate-200"
+                  />
+                ),
+                uncheckedIcon: (
+                  <Icon
+                    name="unchecked"
+                    size={20}
+                    fill="fill-slate-700 dark:fill-slate-200"
+                  />
+                ),
+                leafIcon: (
+                  <Icon
+                    name="file"
+                    size={20}
+                    fill="fill-blue-400 dark:fill-blue-700"
+                  />
+                ),
+                parentIcon: (
+                  <Icon
+                    name="folder"
+                    size={20}
+                    fill="fill-orange-400 dark:fill-yellow-300"
+                  />
+                ),
+                hiddenIcon: (
+                  <Icon
+                    name="square"
+                    size={20}
+                    fill="fill-neutral-200 dark:fill-gray-950"
+                  />
+                ),
+                loadingIcon: (
+                  <Icon
+                    name="loading"
+                    size={20}
+                    fill="animate-spin fill-slate-700 dark:fill-slate-700"
+                  />
+                ),
+              }}
+            />
+          </div>
+        )}
+      </AutoSizer>
     </div>
   );
 }
