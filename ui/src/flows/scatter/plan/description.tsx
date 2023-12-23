be7a5d4b166b0ce71fef68bbe4cb2839bd95326d
Juan B. Rodriguez <jbrodriguez@gmail.com>
2023-12-23 13:37:54 -0500

    chore: small ui changes

--- ui/src/flows/scatter/plan/description.tsx
+++ ui/src/flows/scatter/plan/description.tsx
@@ -4,7 +4,7 @@ export const Description: React.FunctionComponent = () => {
   return (
     <span>
       Monitor the progress of the scatter planning operation. It will show you
-      of any folders/files that can't be transferred due to size constraints (if
+      any folders/files that can't be transferred due to size constraints (if
       any).
     </span>
   );
