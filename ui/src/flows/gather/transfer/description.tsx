054bc6d4391924205ede46733a17e6f60b8802db
Juan B. Rodriguez <jbrodriguez@gmail.com>
2023-12-27 16:47:20 -0500

    chore: small ui change

--- ui/src/flows/gather/transfer/description.tsx
+++ ui/src/flows/gather/transfer/description.tsx
@@ -8,7 +8,7 @@ export const Description: React.FunctionComponent = () => {
       <span className="font-bold">MOVE</span>
       <br />
       <span className="font-bold">*NOTE</span>: Drives are ordered by the least
-      amount of data transfer. An asterisk next to a disk means that one or more
+      amount of data transfer. A star next to a disk means that one or more
       source folders are present. Some folders may not be picked up due to free
       space constraints.
     </span>
