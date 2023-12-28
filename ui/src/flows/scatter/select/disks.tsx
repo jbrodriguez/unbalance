c21b7e12d29fbe0509bc1b9e2977e3f9941e9079
Juan B. Rodriguez <jbrodriguez@gmail.com>
2023-12-28 18:49:07 -0500

    feat: experiment with header styles

--- ui/src/flows/scatter/select/disks.tsx
+++ ui/src/flows/scatter/select/disks.tsx
@@ -16,25 +16,33 @@ export const Disks: React.FunctionComponent = () => {
   const onDiskClick = (disk: IDisk) => setSource(disk.name);
 
   return (
-    <AutoSizer disableWidth>
-      {({ height }) => (
-        <div className="flex flex-1 flex-col bg-neutral-100 dark:bg-gray-950">
-          <div
-            className="overflow-y-auto px-2 pt-2"
-            style={{ height: `${height}px` }}
-          >
-            {disks.map((disk) => (
-              <Selectable
-                disk={disk}
-                onSelectDisk={onDiskClick}
-                selected={disk.name === selected}
-              >
-                <Disk disk={disk} />
-              </Selectable>
-            ))}
+    <div className="h-full bg-neutral-100 dark:bg-gray-950">
+      <div className="flex flex-col p-2">
+        <span className="text-lg font-bold text-slate-500 dark:text-gray-500">
+          Source Disk
+        </span>
+        <span className="border-b pt-2 px-2 border-slate-300 dark:border-gray-700" />
+      </div>
+      <AutoSizer disableWidth>
+        {({ height }) => (
+          <div className="flex flex-1 flex-col">
+            <div
+              className="overflow-y-auto px-2 pt-2"
+              style={{ height: `${height}px` }}
+            >
+              {disks.map((disk) => (
+                <Selectable
+                  disk={disk}
+                  onSelectDisk={onDiskClick}
+                  selected={disk.name === selected}
+                >
+                  <Disk disk={disk} />
+                </Selectable>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </AutoSizer>
+        )}
+      </AutoSizer>
+    </div>
   );
 };
--- ui/src/flows/scatter/select/filesystem.tsx
+++ ui/src/flows/scatter/select/filesystem.tsx
@@ -15,79 +15,86 @@ export const FileSystem: React.FunctionComponent = () => {
   const onCheck = (node: Node) => toggleSelected(node);
 
   return (
-    <AutoSizer disableWidth>
-      {({ height }) => (
-        <div className="flex flex-1 flex-col bg-neutral-100 dark:bg-gray-950">
-          <div
-            className="overflow-y-auto overflow-x-auto p-2 text-slate-700 dark:text-gray-300 "
-            style={{ height: `${height}px` }}
-          >
-            <CheckboxTree
-              nodes={tree}
-              onLoad={onLoad}
-              onCheck={onCheck}
-              icons={{
-                collapseIcon: (
-                  <Icon
-                    name="minus"
-                    size={20}
-                    fill="fill-slate-500 dark:fill-gray-700"
-                  />
-                ),
-                expandIcon: (
-                  <Icon
-                    name="plus"
-                    size={20}
-                    fill="fill-slate-500 dark:fill-gray-700"
-                  />
-                ),
-                checkedIcon: (
-                  <Icon
-                    name="checked"
-                    size={20}
-                    fill="fill-green-700 dark:fill-lime-600"
-                  />
-                ),
-                uncheckedIcon: (
-                  <Icon
-                    name="unchecked"
-                    size={20}
-                    fill="fill-slate-700 dark:fill-slate-200"
-                  />
-                ),
-                leafIcon: (
-                  <Icon
-                    name="file"
-                    size={20}
-                    fill="fill-blue-400 dark:fill-gray-700"
-                  />
-                ),
-                parentIcon: (
-                  <Icon
-                    name="folder"
-                    size={20}
-                    fill="fill-orange-400 dark:fill-gray-700"
-                  />
-                ),
-                hiddenIcon: (
-                  <Icon
-                    name="square"
-                    size={20}
-                    fill="fill-neutral-100 dark:fill-gray-950"
-                  />
-                ),
-                loadingIcon: (
-                  <Icon
-                    name="loading"
-                    size={20}
-                    fill="animate-spin fill-slate-700 dark:fill-slate-700"
-                  />
-                ),
-              }}
-            />
+    <div className="h-full bg-neutral-100 dark:bg-gray-950">
+      <div className="flex flex-col pt-2 px-2">
+        <span className="text-lg px-2 bg-slate-200 dark:bg-gray-800 text-slate-500 dark:text-gray-950">
+          Folders/Files
+        </span>
+      </div>
+      <AutoSizer disableWidth>
+        {({ height }) => (
+          <div className="flex flex-1 flex-col">
+            <div
+              className="overflow-y-auto overflow-x-auto p-2 text-slate-700 dark:text-gray-300 "
+              style={{ height: `${height}px` }}
+            >
+              <CheckboxTree
+                nodes={tree}
+                onLoad={onLoad}
+                onCheck={onCheck}
+                icons={{
+                  collapseIcon: (
+                    <Icon
+                      name="minus"
+                      size={20}
+                      fill="fill-slate-500 dark:fill-gray-700"
+                    />
+                  ),
+                  expandIcon: (
+                    <Icon
+                      name="plus"
+                      size={20}
+                      fill="fill-slate-500 dark:fill-gray-700"
+                    />
+                  ),
+                  checkedIcon: (
+                    <Icon
+                      name="checked"
+                      size={20}
+                      fill="fill-green-700 dark:fill-lime-600"
+                    />
+                  ),
+                  uncheckedIcon: (
+                    <Icon
+                      name="unchecked"
+                      size={20}
+                      fill="fill-slate-700 dark:fill-slate-200"
+                    />
+                  ),
+                  leafIcon: (
+                    <Icon
+                      name="file"
+                      size={20}
+                      fill="fill-blue-400 dark:fill-gray-700"
+                    />
+                  ),
+                  parentIcon: (
+                    <Icon
+                      name="folder"
+                      size={20}
+                      fill="fill-orange-400 dark:fill-gray-700"
+                    />
+                  ),
+                  hiddenIcon: (
+                    <Icon
+                      name="square"
+                      size={20}
+                      fill="fill-neutral-100 dark:fill-gray-950"
+                    />
+                  ),
+                  loadingIcon: (
+                    <Icon
+                      name="loading"
+                      size={20}
+                      fill="animate-spin fill-slate-700 dark:fill-slate-700"
+                    />
+                  ),
+                }}
+              />
+            </div>
           </div>
-        </div>
-      )}
-    </AutoSizer>
+        )}
+      </AutoSizer>
+    </div>
   );
 };
