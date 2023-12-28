c19184fd259b7b9feb54f79436ffaf650e7d753b
Juan B. Rodriguez <jbrodriguez@gmail.com>
2023-12-28 15:28:03 -0500

    chore: small ui change

--- ui/src/shared/disk/base-disk.tsx
+++ ui/src/shared/disk/base-disk.tsx
@@ -10,20 +10,19 @@ type DiskProps = {
 export const Disk: React.FunctionComponent<DiskProps> = ({ disk }) => {
   return (
     <div className="flex flex-col text-blue-800">
-      <div>
-        <span className="font-bold">{disk.name}</span>
-        <span className="dark:text-slate-700 text-slate-500 text-sm">
-          &nbsp;({disk.fsType})
-        </span>{' '}
-        <span className="dark:text-gray-900 text-neutral-400">-</span>{' '}
-        <span className="text-sm">
-          <span className="dark:text-slate-500 text-slate-700 text-sm">
-            {humanBytes(disk.free)}{' '}
+      <div className="flex flex-1 flex-row items-center justify-between">
+        <div className="flex flex-1 flex-row items-center">
+          <span className="font-bold">{disk.name}</span>
+          <span className="dark:text-slate-700 text-slate-500 text-sm">
+            &nbsp;({disk.fsType})
           </span>
-          <span className="dark:text-gray-900 text-neutral-400">/</span>{' '}
+          <span className="px-1">-</span>
           <span className="dark:text-slate-700 text-slate-500">
             {humanBytes(disk.size)}
           </span>
+        </div>
+        <span className="dark:text-slate-500 text-slate-700">
+          {humanBytes(disk.free)}
         </span>
       </div>
       <p className="dark:text-indigo-500 text-indigo-500 text-sm">
