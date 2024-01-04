56a24db7a377d0646eb8ad5c272d956e92ef6e3f
Juan B. Rodriguez <jbrodriguez@gmail.com>
2024-01-04 09:43:18 -0500

    chore: fix promo box

--- ui/src/flows/settings/settings.tsx
+++ ui/src/flows/settings/settings.tsx
@@ -41,7 +41,6 @@ export const Settings: React.FunctionComponent = () => {
                 Reserved Space
               </NavLink>
             </li>
-
             <li>
               <NavLink
                 to="flags"
@@ -57,7 +56,6 @@ export const Settings: React.FunctionComponent = () => {
                 Rsync Flags
               </NavLink>
             </li>
-
             <li>
               <NavLink
                 to="verbosity"
@@ -74,39 +72,17 @@ export const Settings: React.FunctionComponent = () => {
               </NavLink>
             </li>
           </ul>
-          <div className="p-4 mt-6 rounded-lg bg-gray-400 dark:bg-gray-700">
+          <div className="pt-6" />
+          <div className="p-4 rounded-lg border border-gray-400 dark:border-gray-700">
             <div className="flex items-center mb-3">
-              <span className="bg-green-500 text-green-100 text-sm font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-green-800 dark:text-green-200">
+              <span className="bg-green-500 text-green-100 text-sm font-semibold px-2.5 py-0.5 rounded dark:bg-green-800 dark:text-green-200">
                 Promo
               </span>
-              <button
-                type="button"
-                className="ms-auto -mx-1.5 -my-1.5 bg-blue-50 inline-flex justify-center items-center w-6 h-6 text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800"
-                data-dismiss-target="#dropdown-cta"
-                aria-label="Close"
-              >
-                <span className="sr-only">Close</span>
-                <svg
-                  className="w-2.5 h-2.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                  />
-                </svg>
-              </button>
             </div>
-            <p className="mb-3 text-sm text-gray-400 dark:text-gray-200">
-              For a limited time only, donate to unbalance support fund to
-              encourage continuous development of the app ! <br />
-              😀 🙌
+            <p className="mb-3 text-sm text-slate-700 dark:text-gray-300">
+              Donate to the support fund to encourage continuous development.
+              <br />
+              Receive heartfelt thanks from the developer ! 😀 🙌
             </p>
             <a
               className="text-sm text-blue-800 underline font-medium hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-300"
