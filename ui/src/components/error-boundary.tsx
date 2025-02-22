c9b6a184bdf7cb9830fb0bf0509f2b674d5e0297
Juan B. Rodriguez <jbrodriguez@users.noreply.github.com>
2025-02-22 18:10:08 -0500

    fix build warning (#123)

--- ui/src/components/error-boundary.tsx
+++ ui/src/components/error-boundary.tsx
@@ -14,7 +14,7 @@ class ErrorBoundary extends Component<Props, State> {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError(_: Error) {
     // Update state so the next render will show the fallback UI
     return { hasError: true };
   }
